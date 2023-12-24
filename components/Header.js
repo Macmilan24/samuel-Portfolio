4ddc02ec8a18184406835c330c98dd9eaf4d504a
Macmilan24 <cpt.macmilan24@gmail.com>
2023-12-24 19:46:31 +0300

    first commit

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ import Socials from '../components/Socials';
 const Header = () => {
   return(
    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
-    <div className="container mx-auto">
+    <div className="container mx-auto md:mb-12 sm:mb-8 lg:mb-8">
       <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
         {/* logo */}
         <Link href={'/'}>
